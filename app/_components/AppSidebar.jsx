7afb4e8735e088df52689811cb489cf37b1626e7
Prashant <prashan1837d@gmail.com>
2025-05-23 02:32:25 +0530

    SideBar init

--- app/_components/AppSidebar.jsx
+++ app/_components/AppSidebar.jsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
   SidebarContent,
@@ -8,8 +11,9 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { BookCopy, LogIn, Orbit, Search } from "lucide-react";
+import { BookCopy, Ghost, LogIn, Orbit, Search } from "lucide-react";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const MenuOptions = [
   {
@@ -20,21 +24,22 @@ const MenuOptions = [
   {
     title: "Discover",
     icon: Orbit,
-    path: "/",
+    path: "/discover",
   },
   {
     title: "Library",
     icon: BookCopy,
-    path: "/",
+    path: "/library",
   },
   {
     title: "Sign In",
     icon: LogIn,
-    path: "/",
+    path: "#",
   },
 ];
 
 export function AppSidebar() {
+  const path = usePathname();
   return (
     <Sidebar>
       <SidebarHeader className="bg-accent flex items-center py-5">
@@ -46,20 +51,31 @@ export function AppSidebar() {
             <SidebarMenu>
               {MenuOptions.map((menu, index) => (
                 <SidebarMenuItem key={index}>
-                  <SidebarMenuButton asChild className={`p-5 py-5 hover:bg-transparent hover:font-bold `}>
+                  <SidebarMenuButton
+                    asChild
+                    className={`p-5 py-5 hover:bg-transparent hover:font-bold ${path?.includes(menu.path) && 'font-bold'}
+                    `}
+                  >
                     <a href={menu.path} className="">
-                      <menu.icon className="h-8 w-8"/>
-                      <span className="text-lg ">{menu.title}</span>
+                      <menu.icon className="h-8 w-8" />
+                      <span className="text-[1.1rem] ">{menu.title}</span>
                     </a>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
             </SidebarMenu>
+            <Button className="rounded-full mx-4 my-4">Sign Up</Button>
           </SidebarContent>
         </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter className="bg-accent" />
+      <SidebarFooter className="bg-accent">
+        <div className="p-3 ">
+            <h2 className="text-gray-500 ">Try Now</h2>
+            <p className="text-gray-400">Upgrade for image upload, Smarter AI,and more Copilot</p>
+            <Button variant={"link"} className="text-gray-500">Learn More</Button>
+        </div>
+      </SidebarFooter>
     </Sidebar>
   );
 }
