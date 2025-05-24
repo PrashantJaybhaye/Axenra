01944a48c7c96eb4650cc87dd686b2bd214503b2
Prashant <prashan1837d@gmail.com>
2025-05-25 05:10:35 +0530

    feat: add dropdown menu and AI model options to ChatBoxInput component

--- app/_components/ChatBoxInput.jsx
+++ app/_components/ChatBoxInput.jsx
@@ -11,14 +11,23 @@ import {
   SearchCheck,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AiModelsOption, ReasoningModels } from "@/services/Shared";
 
 function ChatBoxInput() {
   return (
     <div className="flex flex-col h-screen items-center justify-center w-full -ml-6 ">
-      <Image 
-        src={"/raw2.png"} 
-        alt="Logo" 
-        width={250} 
+      <Image
+        src={"/raw2.png"}
+        alt="Logo"
+        width={250}
         height={250}
         className="w-[300px] sm:w-[250px] md:w-[250px]"
       />
@@ -39,30 +48,92 @@ function ChatBoxInput() {
                 className="w-full p-3 sm:p-4 outline-none border-none sm:text-base"
               />
             </TabsContent>
-            <TabsList className='flex max-sm:w-full'>
+            <TabsList className="flex max-sm:w-full">
               <TabsTrigger value="Search" className="text-primary">
                 <SearchCheck className="size-4 sm:size-5" /> Search
               </TabsTrigger>
               <TabsTrigger value="Research" className="text-primary">
-                <AtomIcon className="size-4 sm:size-5"/> Research
+                <AtomIcon className="size-4 sm:size-5" /> Research
               </TabsTrigger>
             </TabsList>
           </Tabs>
 
           <div className="flex items-center justify-end gap-0 sm:gap-1">
-            <Button variant="ghost">
-              <Globe className="text-gray-500 size-4 sm:size-5" />
-            </Button>
-            <Button variant="ghost">
-              <Paperclip className="text-gray-500 size-4 sm:size-5" />
-            </Button>
-            <Button variant="ghost">
-              <Mic className="text-gray-500 size-4 sm:size-5" />
-            </Button>
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <div className="relative group inline-block">
+                  <Button variant="ghost">
+                    <Cpu className="text-gray-500 size-4 sm:size-5" />
+                  </Button>
+                  <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs sm:text-xs bg-black text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                    Choose a model
+                  </div>
+                </div>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="overflow-y-auto">
+                <DropdownMenuLabel>
+                  <span className="text-primary">Best</span>
+                  <p className="text-xs text-gray-500">
+                    Select the best model for each query
+                  </p>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {AiModelsOption.map((model, index) => (
+                  <DropdownMenuItem key={index}>
+                    <div className="mb-1">
+                      <h2 className="text-sm">{model.name}</h2>
+                      <p className="text-xs">{model.desc}</p>
+                    </div>
+                  </DropdownMenuItem>
+                ))}
+                {/* <DropdownMenuSeparator />
+                <p className="text-xs">Reasoning</p>
+                {ReasoningModels.map((model, index) => (
+                  <DropdownMenuItem key={index}>
+                    <div className="mb-1">
+                      <h2 className="text-sm">{model.name}</h2>
+                      <p className="text-xs">{model.desc}</p>
+                    </div>
+                  </DropdownMenuItem>
+                ))} */}
+              </DropdownMenuContent>
+            </DropdownMenu>
 
-            <Button>
-              <AudioLines className="text-white size-4 sm:size-5" />
-            </Button>
+            <div className="relative group inline-block">
+              <Button variant="ghost">
+                <Globe className="text-gray-500 size-4 sm:size-5" />
+              </Button>
+              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs sm:text-xs bg-black text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                Set sources for search
+              </div>
+            </div>
+
+            <div className="relative group inline-block">
+              <Button variant="ghost">
+                <Paperclip className="text-gray-500 size-4 sm:size-5" />
+              </Button>
+              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs sm:text-xs bg-black text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                Attach File
+              </div>
+            </div>
+
+            <div className="relative group inline-block">
+              <Button variant="ghost">
+                <Mic className="text-gray-500 size-4 sm:size-5" />
+              </Button>
+              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs sm:text-xs bg-black text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                Dictation
+              </div>
+            </div>
+
+            <div className="relative group inline-block">
+              <Button>
+                <AudioLines className="text-white size-4 sm:size-5" />
+              </Button>
+              <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 w-max px-2 py-1 text-xs sm:text-xs bg-black text-white rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
+                Voice Mode
+              </div>
+            </div>
           </div>
         </div>
       </div>
---
+++ services/Shared.jsx
@@ -0,0 +1,48 @@
+export const AiModelsOption = [
+  {
+    id: 1,
+    name: "Sonar",
+    desc: "Axenra's fast model"
+  },
+  {
+    id: 2,
+    name: "Claude 4.0 Sonnet",
+    desc: "Anthropic's advance model"
+  },
+  {
+    id: 3,
+    name: "GPT-4.1",
+    desc: "OpenAI's advance model"
+  },
+  {
+    id: 4,
+    name: "Gemini 2.5 Pro",
+    desc: "Google's latest model"
+  },
+  {
+    id: 5,
+    name: "Grok 3 Beta",
+    desc: "xAI's latest model"
+  },
+  
+];
+
+export const ReasoningModels = [
+  {
+    id:1,
+    name: "R1 1776",
+    desc: "Axenra\'s unbiased reasoning model"
+  },
+  
+  {
+    id:2,
+    name: "o4-mini",
+    desc: "OpenAi\'s latest reasoning model "
+  },
+  {
+    id:3,
+    name: "Claude 4.0 Sonnet Thinking",
+    desc: "Anthropic\'s reasoning model"
+  },
+
+]
