96278a15404f4d0b1d7a12cf7f7a1e5b56135515
Prashant <prashan1837d@gmail.com>
2025-05-29 01:43:05 +0530

    Refactor model descriptions for consistency and add SEARCH_RESULT structure

--- services/Shared.jsx
+++ services/Shared.jsx
@@ -2,48 +2,409 @@ export const AiModelsOption = [
   {
     id: 1,
     name: "Sonar",
-    desc: "Axenra's fast model"
-  
+    desc: "Axenra's fast model",
   },
   {
     id: 2,
     name: "Claude 4.0 Sonnet",
-    desc: "Anthropic's advance model"
+    desc: "Anthropic's advance model",
   },
   {
     id: 3,
     name: "GPT-4.1",
-    desc: "OpenAI's advance model"
+    desc: "OpenAI's advance model",
   },
   {
     id: 4,
     name: "Gemini 2.5 Pro",
-    desc: "Google's latest model"
+    desc: "Google's latest model",
   },
   {
     id: 5,
     name: "Grok 3 Beta",
-    desc: "xAI's latest model"
+    desc: "xAI's latest model",
   },
-  
 ];
 
 export const ReasoningModels = [
   {
-    id:1,
+    id: 1,
     name: "R1 1776",
-    desc: "Axenra\'s unbiased reasoning model"
-  },
-  
-  {
-    id:2,
-    name: "o4-mini",
-    desc: "OpenAi\'s latest reasoning model "
-  },
-  {
-    id:3,
-    name: "Claude 4.0 Sonnet Thinking",
-    desc: "Anthropic\'s reasoning model"
+    desc: "Axenra's unbiased reasoning model",
   },
 
-]
+  {
+    id: 2,
+    name: "o4-mini",
+    desc: "OpenAi's latest reasoning model ",
+  },
+  {
+    id: 3,
+    name: "Claude 4.0 Sonnet Thinking",
+    desc: "Anthropic's reasoning model",
+  },
+];
+
+export const SEARCH_RESULT = {
+  query: {
+    original: "what is youtube?",
+    show_strict_warning: false,
+    is_navigational: false,
+    is_news_breaking: false,
+    spellcheck_off: true,
+    country: "us",
+    bad_results: false,
+    should_fallback: false,
+    postal_code: "",
+    city: "",
+    header_country: "",
+    more_results_available: true,
+    state: "",
+  },
+  mixed: {
+    type: "mixed",
+    main: [
+      { type: "web", index: 0, all: false },
+      { type: "web", index: 1, all: false },
+      { type: "videos", all: true },
+      { type: "web", index: 2, all: false },
+      { type: "web", index: 3, all: false },
+      { type: "web", index: 4, all: false },
+      { type: "web", index: 5, all: false },
+      { type: "web", index: 6, all: false },
+      { type: "web", index: 7, all: false },
+      { type: "web", index: 8, all: false },
+      { type: "web", index: 9, all: false },
+      { type: "web", index: 10, all: false },
+      { type: "web", index: 11, all: false },
+      { type: "web", index: 12, all: false },
+      { type: "web", index: 13, all: false },
+      { type: "web", index: 14, all: false },
+      { type: "web", index: 15, all: false },
+      { type: "web", index: 16, all: false },
+      { type: "web", index: 17, all: false },
+      { type: "web", index: 18, all: false },
+      { type: "web", index: 19, all: false },
+    ],
+    top: [],
+    side: [],
+  },
+  type: "search",
+  videos: {
+    type: "videos",
+    results: [
+      {
+        type: "video_result",
+        url: "https://m.youtube.com/watch?v=IlFmBbeKRXQ",
+        title: "What Actually Happens When You Watch A YouTube Video?",
+        description: "Share your videos with friends, family, and the world",
+        video: {},
+        meta_url: {
+          scheme: "https",
+          netloc: "m.youtube.com",
+          hostname: "m.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/p0B86IaQr1YuFWzlQTuEltmxPAh7UzF2DpyN8i96T6M/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvZTExOWJhZDk1/ZTUxZWUwYmE3NTg0/N2QzZjQwZjZhMTE5/ZTc1NWY5MzMwYjll/OTJhNTM3ZjdmZTMw/MzdlM2EwNC9tLnlv/dXR1YmUuY29tLw",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/5AWwrvS1fnGrbe_4CUy-IlGmWPPuJF0WLjNWgnfGUVQ/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9J/bEZtQmJlS1JYUS9o/cWRlZmF1bHQuanBn",
+          original: "https://i.ytimg.com/vi/IlFmBbeKRXQ/hqdefault.jpg",
+        },
+      },
+      {
+        type: "video_result",
+        url: "https://m.youtube.com/watch?v=a3mxjQUZ2WU",
+        title: "What is YouTube TV? | Explanation of What YouTube TV is ...",
+        description: "Share your videos with friends, family, and the world",
+        video: {},
+        meta_url: {
+          scheme: "https",
+          netloc: "m.youtube.com",
+          hostname: "m.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/p0B86IaQr1YuFWzlQTuEltmxPAh7UzF2DpyN8i96T6M/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvZTExOWJhZDk1/ZTUxZWUwYmE3NTg0/N2QzZjQwZjZhMTE5/ZTc1NWY5MzMwYjll/OTJhNTM3ZjdmZTMw/MzdlM2EwNC9tLnlv/dXR1YmUuY29tLw",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/YLs11GyqSiFsSHYVEGlBz80NpkqzrV0m1FP8FJqZknE/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9h/M214alFVWjJXVS9o/cWRlZmF1bHQuanBn",
+          original: "https://i.ytimg.com/vi/a3mxjQUZ2WU/hqdefault.jpg",
+        },
+      },
+      {
+        type: "video_result",
+        url: "https://www.youtube.com/watch?v=qBiZA4QDsj0",
+        title: "What Is Youtube Red? Should I Buy Youtube Red?",
+        description:
+          "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on YouTube.",
+        video: {},
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/CA93O-HQi_vWoBoHqmw2eP8zh-pHXiAzeB4XNpaMUVw/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9x/QmlaQTRRRHNqMC9o/cWRlZmF1bHQuanBn",
+          original: "https://i.ytimg.com/vi/qBiZA4QDsj0/hqdefault.jpg",
+        },
+      },
+      {
+        type: "video_result",
+        url: "https://www.youtube.com/watch?v=q4Bw6mNJ2ss",
+        title: "Why Is YouTube So Hard? - YouTube",
+        description:
+          "🚀 Grow your channel using the best YouTube tools: https://vidiq.ink/growIn this video we talk about why every wants to do YouTube and become a creator but a",
+        age: "March 7, 2025",
+        page_age: "2025-03-07T13:45:05",
+        video: {
+          duration: "14:31",
+          views: 5390,
+          creator: "vidIQ",
+          publisher: "YouTube",
+        },
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/UQGGr3m5o09puhBYZuDlRd4K4iL1lxN31BYBj0RefKw/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9x/NEJ3Nm1OSjJzcy9t/YXhyZXNkZWZhdWx0/LmpwZw",
+          original: "https://i.ytimg.com/vi/q4Bw6mNJ2ss/maxresdefault.jpg",
+        },
+      },
+      {
+        type: "video_result",
+        url: "https://www.youtube.com/watch?v=dvV6x2BQoXk&pp=0gcJCdgAo7VqN5tD",
+        title: "What is YouTube? - YouTube",
+        description:
+          "We went to Summer in the City 2014 and asked a load of the creators to sum up what YouTube is.Subscribe to The Guardian ► http://bitly.com/UvkFpDFeaturing:Sa...",
+        age: "August 18, 2014",
+        page_age: "2014-08-18T14:41:32",
+        video: {
+          duration: "01:41",
+          views: 48989,
+          creator: "The Guardian",
+          publisher: "YouTube",
+        },
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/q1E9DpVbJLFEAb6cTBDzJommjnBasiRp_VAiBd56OmQ/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9k/dlY2eDJCUW9Yay9t/YXhyZXNkZWZhdWx0/LmpwZz9zcXA9LW9h/eW13RW1DSUFLRU5B/RjhxdUtxUU1hOEFF/Qi1BSC1DWUFDMEFX/S0Fnd0lBQkFCR0dV/Z1dpaE5NQTg9JmFt/cDtycz1BT240Q0xE/WlBSanhTZHZtUWRT/a00zc3BTZlVIczBk/UC1R",
+          original:
+            "https://i.ytimg.com/vi/dvV6x2BQoXk/maxresdefault.jpg?sqp=-oaymwEmCIAKENAF8quKqQMa8AEB-AH-CYAC0AWKAgwIABABGGUgWihNMA8=&amp;rs=AOn4CLDZPRjxSdvmQdSkM3spSfUHs0dP-Q",
+        },
+      },
+      {
+        type: "video_result",
+        url: "https://www.youtube.com/watch?v=meMJdfytNI0",
+        title:
+          "How to Start a YouTube Channel - Beginners Guide (2024) - YouTube",
+        description:
+          "Use the promo code ALI to get a free share worth up to £100 or visit: https://www.trading212.com/join/ALIThis video does not represent financial advice, and ...",
+        age: "October 4, 2024",
+        page_age: "2024-10-04T13:01:00",
+        video: {
+          duration: "39:59",
+          views: 209279,
+          creator: "Ali Abdaal",
+          publisher: "YouTube",
+        },
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "› watch",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/qov2Q2GfdhmrFpSL2BGZrKt86caNlf-9oSk68kfb2wk/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9pLnl0/aW1nLmNvbS92aS9t/ZU1KZGZ5dE5JMC9t/YXhyZXNkZWZhdWx0/LmpwZw",
+          original: "https://i.ytimg.com/vi/meMJdfytNI0/maxresdefault.jpg",
+        },
+      },
+    ],
+    mutated_by_goggles: false,
+  },
+  web: {
+    type: "search",
+    results: [
+      {
+        title: "YouTube - Wikipedia",
+        url: "https://en.wikipedia.org/wiki/YouTube",
+        is_source_local: false,
+        is_source_both: false,
+        description:
+          "YouTube is <strong>an American social media and online video sharing platform owned by Google</strong>. YouTube was founded on February 14, 2005, by Steve Chen, Chad Hurley, and Jawed Karim, three former employees of PayPal. Headquartered in San Bruno, California, it is the second-most-visited website in the ...",
+        page_age: "2025-05-28T11:41:31",
+        profile: {
+          name: "Wikipedia",
+          url: "https://en.wikipedia.org/wiki/YouTube",
+          long_name: "en.wikipedia.org",
+          img: "https://imgs.search.brave.com/m6XxME4ek8DGIUcEPCqjRoDjf2e54EwL9pQzyzogLYk/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvNjQwNGZhZWY0/ZTQ1YWUzYzQ3MDUw/MmMzMGY3NTQ0ZjNj/NDUwMDk5ZTI3MWRk/NWYyNTM4N2UwOTE0/NTI3ZDQzNy9lbi53/aWtpcGVkaWEub3Jn/Lw",
+        },
+        language: "en",
+        family_friendly: true,
+        type: "search_result",
+        subtype: "generic",
+        is_live: false,
+        meta_url: {
+          scheme: "https",
+          netloc: "en.wikipedia.org",
+          hostname: "en.wikipedia.org",
+          favicon:
+            "https://imgs.search.brave.com/m6XxME4ek8DGIUcEPCqjRoDjf2e54EwL9pQzyzogLYk/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvNjQwNGZhZWY0/ZTQ1YWUzYzQ3MDUw/MmMzMGY3NTQ0ZjNj/NDUwMDk5ZTI3MWRk/NWYyNTM4N2UwOTE0/NTI3ZDQzNy9lbi53/aWtpcGVkaWEub3Jn/Lw",
+          path: "› wiki  › YouTube",
+        },
+        age: "8 hours ago",
+      },
+      {
+        title: "YouTube: What is YouTube?",
+        url: "https://edu.gcfglobal.org/en/youtube/what-is-youtube/1/",
+        is_source_local: false,
+        is_source_both: false,
+        description:
+          "YouTube is <strong>a free video sharing website that makes it easy to watch online videos</strong>. You can even create and upload your own videos to share with others.",
+        profile: {
+          name: "Gcfglobal",
+          url: "https://edu.gcfglobal.org/en/youtube/what-is-youtube/1/",
+          long_name: "edu.gcfglobal.org",
+          img: "https://imgs.search.brave.com/Wbg4HU0rAspMB3MOxqOFO_6o5Ad_J5DpQeiq4_DWpfs/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTE5ZWEyOTI0/YmJjNDJmZDQ2MzEy/Y2Y1YjM2YTkxYmI3/MzJmYTQ1YjQyZTkx/MjAzNWQxZTBiNjA1/OGNkMjI3Ni9lZHUu/Z2NmZ2xvYmFsLm9y/Zy8",
+        },
+        language: "en",
+        family_friendly: true,
+        type: "search_result",
+        subtype: "generic",
+        is_live: false,
+        meta_url: {
+          scheme: "https",
+          netloc: "edu.gcfglobal.org",
+          hostname: "edu.gcfglobal.org",
+          favicon:
+            "https://imgs.search.brave.com/Wbg4HU0rAspMB3MOxqOFO_6o5Ad_J5DpQeiq4_DWpfs/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTE5ZWEyOTI0/YmJjNDJmZDQ2MzEy/Y2Y1YjM2YTkxYmI3/MzJmYTQ1YjQyZTkx/MjAzNWQxZTBiNjA1/OGNkMjI3Ni9lZHUu/Z2NmZ2xvYmFsLm9y/Zy8",
+          path: "› en  › youtube  › what-is-youtube  › 1",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/Dg0XH_zDOeHqwpQoTcB5uE_Xyd-eFTeIvQ-tzcuvABs/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nY2ZsZWFybmZy/ZWUub3JnL2dsb2Jh/bC9sYXlvdXQvc29j/aWFsL2djZi13ZWJz/aXRlLWltYWdlLnBu/Zw",
+          original:
+            "https://media.gcflearnfree.org/global/layout/social/gcf-website-image.png",
+          logo: false,
+        },
+      },
+      {
+        title: "Explained: What is YouTube?",
+        url: "https://www.webwise.ie/parents/what-is-youtube/",
+        is_source_local: false,
+        is_source_both: false,
+        description:
+          "<strong>YouTube</strong> <strong>is</strong> a video sharing service where users can create their own profile, upload videos, watch, like and comment on other videos.",
+        page_age: "2021-08-04T14:06:45",
+        profile: {
+          name: "Webwise",
+          url: "https://www.webwise.ie/parents/what-is-youtube/",
+          long_name: "webwise.ie",
+          img: "https://imgs.search.brave.com/emA8WmPjwBj8yIlMAFjRArMm4u4h_i9I834pF67GG8A/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMDZmNThmMDMx/NjhkZTVjNjVmOTU2/NzEwMjMzMzMwMGRk/NzY0MmEzODIwY2U5/MDA5N2U0ZDg4MzAw/N2JiOTg1My93d3cu/d2Vid2lzZS5pZS8",
+        },
+        language: "en",
+        family_friendly: true,
+        type: "search_result",
+        subtype: "generic",
+        is_live: false,
+        meta_url: {
+          scheme: "https",
+          netloc: "webwise.ie",
+          hostname: "www.webwise.ie",
+          favicon:
+            "https://imgs.search.brave.com/emA8WmPjwBj8yIlMAFjRArMm4u4h_i9I834pF67GG8A/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvMDZmNThmMDMx/NjhkZTVjNjVmOTU2/NzEwMjMzMzMwMGRk/NzY0MmEzODIwY2U5/MDA5N2U0ZDg4MzAw/N2JiOTg1My93d3cu/d2Vid2lzZS5pZS8",
+          path: "  › home  › news  › explained: what is youtube?",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/9IRo_ReGbETLsJ-VlBlR7qMsgNd0QwKrmyJec-1jhns/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly93d3cu/d2Vid2lzZS5pZS93/cC1jb250ZW50L3Vw/bG9hZHMvMjAxNS8x/MC95b3V0dWJlLnBu/Zw",
+          original:
+            "https://www.webwise.ie/wp-content/uploads/2015/10/youtube.png",
+          logo: false,
+        },
+        age: "August 4, 2021",
+      },
+      {
+        title: "YouTube",
+        url: "https://www.youtube.com/",
+        is_source_local: false,
+        is_source_both: false,
+        description:
+          "Enjoy the videos and music you love, upload original content, and share it all with friends, family, and the world on <strong>YouTube</strong>.",
+        page_age: "2008-08-01T00:00:00",
+        profile: {
+          name: "YouTube",
+          url: "https://www.youtube.com/",
+          long_name: "youtube.com",
+          img: "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+        },
+        language: "en",
+        family_friendly: true,
+        type: "search_result",
+        subtype: "generic",
+        is_live: false,
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/f8_af-wBan_wI-S0VxzoJufxUeI1DDuNFZWgi1GDERE/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly93d3cu/eW91dHViZS5jb20v/aW1nL2Rlc2t0b3Av/eXRfMTIwMC5wbmc",
+          original: "https://www.youtube.com/img/desktop/yt_1200.png",
+          logo: false,
+        },
+        age: "August 1, 2008",
+      },
+      {
+        title: "How YouTube Works - Product Features, Responsibility, & Impact",
+        url: "https://www.youtube.com/howyoutubeworks/",
+        is_source_local: false,
+        is_source_both: false,
+        description:
+          "Discover how <strong>YouTube</strong> Products work and what we&#x27;re doing to protect our community so that creativity, diversity, and respect are able to thrive.",
+        profile: {
+          name: "YouTube",
+          url: "https://www.youtube.com/howyoutubeworks/",
+          long_name: "youtube.com",
+          img: "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+        },
+        language: "en",
+        family_friendly: true,
+        type: "search_result",
+        subtype: "generic",
+        is_live: false,
+        meta_url: {
+          scheme: "https",
+          netloc: "youtube.com",
+          hostname: "www.youtube.com",
+          favicon:
+            "https://imgs.search.brave.com/Wg4wjE5SHAargkzePU3eSLmWgVz84BEZk1SjSglJK_U/rs:fit:32:32:1:0/g:ce/aHR0cDovL2Zhdmlj/b25zLnNlYXJjaC5i/cmF2ZS5jb20vaWNv/bnMvOTkyZTZiMWU3/YzU3Nzc5YjExYzUy/N2VhZTIxOWNlYjM5/ZGVjN2MyZDY4Nzdh/ZDYzMTYxNmI5N2Rk/Y2Q3N2FkNy93d3cu/eW91dHViZS5jb20v",
+          path: "› howyoutubeworks",
+        },
+        thumbnail: {
+          src: "https://imgs.search.brave.com/q6GJnPnt4uKlR5g-keF9Hegr-uRcfvUBoweKgvrKI7c/rs:fit:200:200:1:0/g:ce/aHR0cHM6Ly9saDMu/Z29vZ2xldXNlcmNv/bnRlbnQuY29tLzN6/a1AyU1llN3lZb0tL/ZTQ3YnNOZTQ0eVRn/YjRVa2hfX3JCYndY/d2drak5SZTRQeWtH/RzQwOW96Qnh6eGty/dWJWN3pIS2pmeHE2/eTlTaG9nV3RNQk1Q/eUIzamlOcHM5MUxv/Tkg4QT1zNTAw",
+          original:
+            "https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500",
+          logo: false,
+        },
+      },
+    ],
+    family_friendly: true,
+  },
+};
